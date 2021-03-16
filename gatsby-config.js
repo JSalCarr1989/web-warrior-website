16caf2e410e837353649361a1ce1ce6e2f09d62a
Salvador Carrillo <Salvador.Carrillo1@partner.magna.com>
2021-03-16 08:56:27 -0600

    commit avance curso gatsby net ninja

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`, // nombre de la instancia de este plugin
+        path: `${__dirname}/src/projects/`,  // ruta de donde tomara los archivos para agregarlos a la capa de graphql
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`, // nombre de la instancia para imagenes
+        path: `${__dirname}/src/images/`,  // ruta de donde tomara las imagenes
+      },
+    }
+  ],
+  siteMetadata: {
+    title: 'Web Warrior',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright 2021 Web Warrior',
+    contact: 'me@thewebwarrior.co.uk'
+  }
 }
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+    return (
+        <Layout>
+            <div>
+                <h1>About page</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis nisi nulla, quis dictum mauris vehicula in. Praesent eu venenatis libero. Sed eget est in quam ornare bibendum ut quis purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed non ornare velit. Vivamus quis ante ante. Duis imperdiet convallis metus sit amet iaculis.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis nisi nulla, quis dictum mauris vehicula in. Praesent eu venenatis libero. Sed eget est in quam ornare bibendum ut quis purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed non ornare velit. Vivamus quis ante ante. Duis imperdiet convallis metus sit amet iaculis.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sagittis nisi nulla, quis dictum mauris vehicula in. Praesent eu venenatis libero. Sed eget est in quam ornare bibendum ut quis purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed non ornare velit. Vivamus quis ante ante. Duis imperdiet convallis metus sit amet iaculis.</p>
+            </div>
+        </Layout>
+    )
+}
